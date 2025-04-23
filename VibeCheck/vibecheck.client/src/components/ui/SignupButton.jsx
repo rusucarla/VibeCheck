39737e333bc505c1ed79910ff0a50e3b1e3467fc
Scarlett-stack <87925542+Scarlett-stack@users.noreply.github.com>
2025-04-23 14:16:52 +0300

    Update SignupButton.jsx fix pt submit in ui comp {..rest]

--- VibeCheck/vibecheck.client/src/components/ui/SignupButton.jsx
+++ VibeCheck/vibecheck.client/src/components/ui/SignupButton.jsx
@@ -1,23 +1,28 @@
 import Button from "./Button";
 import TheatersIcon from "@mui/icons-material/Theaters"; // cinema
 
-function SignupButton({ onClick }) {
+function SignupButton({ onClick, sx = {}, ...rest }) {
     return (
         <Button
             onClick={onClick}
             color="error"
             startIcon={<TheatersIcon />}
             sx={{
-                backgroundColor: "#c62828", // rosu inchis
+                backgroundColor: "#c62828",
                 color: "#fff",
+                boxShadow: "0 0 10px rgba(255, 82, 82, 0.7)",
                 ":hover": {
-                    backgroundColor: "#b71c1c"
-                }
+                    backgroundColor: "#b71c1c",  //rosu inchis
+                    boxShadow: "0 0 20px rgba(255, 82, 82, 1)"
+                },
+                ...sx
             }}
+            {...rest} //AICI trebuie sa fie inclus `type="submit"` din form
         >
             Sign Up
         </Button>
     );
 }
+
 
 export default SignupButton;
